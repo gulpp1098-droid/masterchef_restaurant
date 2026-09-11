e995440445e04481f7701c8a36bbf4b5fbc46a83
gulpp1098-droid <gulpp1098@gmail.com>
2026-09-11 17:25:51 +0200

    Wrong Item Slot fixes

--- src/main/java/net/mcreator/masterchefrestaurant/procedures/OpenedOrderGUIProcedure.java
+++ src/main/java/net/mcreator/masterchefrestaurant/procedures/OpenedOrderGUIProcedure.java
@@ -7,9 +7,6 @@ public class OpenedOrderGUIProcedure {
 	public static void execute(LevelAccessor world, Entity entity) {
 		if (entity == null)
 			return;
-		if (world.dayTime() % 5 == 0) {
-			ClosingOrderGUIProcedure.execute(world, entity);
-		}
 		PatianceInOrderGUIProcedure.execute(world, entity);
 	}
 }
--- src/main/java/net/mcreator/masterchefrestaurant/procedures/PatianceInOrderGUIProcedure.java
+++ src/main/java/net/mcreator/masterchefrestaurant/procedures/PatianceInOrderGUIProcedure.java
@@ -3,6 +3,7 @@ package net.mcreator.masterchefrestaurant.procedures;
 import net.neoforged.neoforge.network.PacketDistributor;
 
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.level.ServerLevel;
@@ -16,9 +17,24 @@ public class PatianceInOrderGUIProcedure {
 	public static void execute(LevelAccessor world, Entity entity) {
 		if (entity == null)
 			return;
-		if (entity instanceof ServerPlayer player2)
-			PacketDistributor.sendToPlayer(player2, new PatiancePacketToClientMessage((new java.text.DecimalFormat("##")
-					.format((world instanceof ServerLevel _level0 ? getEntityFromUUID(_level0, entity.getData(MasterchefRestaurantModVariables.PLAYER_VARIABLES).CurrentClientUUID) : null).getPersistentData().getDouble("patience")))));
+		Entity client = null;
+		Entity player = null;
+		if (!world.isClientSide()) {
+			client = world instanceof ServerLevel _level1 ? getEntityFromUUID(_level1, entity.getData(MasterchefRestaurantModVariables.PLAYER_VARIABLES).CurrentClientUUID) : null;
+			player = entity;
+			if (client != null) {
+				if (("food_wait").equals(client.getPersistentData().getString("state")) || ("order_wait").equals(client.getPersistentData().getString("state"))) {
+					if (player instanceof ServerPlayer player5)
+						PacketDistributor.sendToPlayer(player5, new PatiancePacketToClientMessage((new java.text.DecimalFormat("##").format(client.getPersistentData().getDouble("patience")))));
+				} else {
+					if (player instanceof Player _player)
+						_player.closeContainer();
+				}
+			} else {
+				if (player instanceof Player _player)
+					_player.closeContainer();
+			}
+		}
 	}
 
 	private static Entity getEntityFromUUID(ServerLevel level, String uuid) {
