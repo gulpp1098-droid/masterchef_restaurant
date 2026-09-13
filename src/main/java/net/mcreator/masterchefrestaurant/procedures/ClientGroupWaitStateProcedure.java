5321ef2d2839e7f2f40ea4fb428493ef6277a3c9
gulpp1098-droid <gulpp1098@gmail.com>
2026-09-13 10:14:26 +0200

    client group search fixes

--- src/main/java/net/mcreator/masterchefrestaurant/procedures/ClientGroupWaitStateProcedure.java
+++ src/main/java/net/mcreator/masterchefrestaurant/procedures/ClientGroupWaitStateProcedure.java
@@ -28,7 +28,7 @@ public class ClientGroupWaitStateProcedure {
 			final Vec3 _center = new Vec3(x, y, z);
 			for (Entity entityiterator : world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(4 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList()) {
 				if (entityiterator instanceof ClientEntity) {
-					if (client.getPersistentData().getDouble("group") == entityiterator.getPersistentData().getDouble("group")) {
+					if (client.getPersistentData().getDouble("group") == entityiterator.getPersistentData().getDouble("group") && client.getPersistentData().getDouble("RestaurantID") == entityiterator.getPersistentData().getDouble("RestaurantID")) {
 						if (!(entityiterator.getPersistentData().getString("state")).equals("group_wait")) {
 							AllReady = false;
 						}
@@ -42,7 +42,8 @@ public class ClientGroupWaitStateProcedure {
 					final Vec3 _center = new Vec3(x, y, z);
 					for (Entity entityiterator : world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(4 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList()) {
 						if (entityiterator instanceof ClientEntity) {
-							if (client.getPersistentData().getDouble("group") == entityiterator.getPersistentData().getDouble("group")) {
+							if (client.getPersistentData().getDouble("group") == entityiterator.getPersistentData().getDouble("group")
+									&& client.getPersistentData().getDouble("RestaurantID") == entityiterator.getPersistentData().getDouble("RestaurantID")) {
 								ClientExpPayProcedure.execute(world, entityiterator);
 								ClientBeginLeavingProcedure.execute(world, entityiterator);
 							}
@@ -54,7 +55,8 @@ public class ClientGroupWaitStateProcedure {
 					final Vec3 _center = new Vec3(x, y, z);
 					for (Entity entityiterator : world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(4 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList()) {
 						if (entityiterator instanceof ClientEntity) {
-							if (client.getPersistentData().getDouble("group") == entityiterator.getPersistentData().getDouble("group")) {
+							if (client.getPersistentData().getDouble("group") == entityiterator.getPersistentData().getDouble("group")
+									&& client.getPersistentData().getDouble("RestaurantID") == entityiterator.getPersistentData().getDouble("RestaurantID")) {
 								ClientCoinPayProcedure.execute(world, entityiterator);
 								ClientExpPayProcedure.execute(world, entityiterator);
 								entityiterator.getPersistentData().putString("state", "leave");
