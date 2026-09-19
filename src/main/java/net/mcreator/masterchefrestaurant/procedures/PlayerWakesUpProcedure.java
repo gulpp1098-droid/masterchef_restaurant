1f5a95506f526dc2e3da77f172e65cb99924dc7c
gulpp1098-droid <gulpp1098@gmail.com>
2026-09-19 11:40:01 +0200

    sleep fixes

---
+++ src/main/java/net/mcreator/masterchefrestaurant/procedures/PlayerWakesUpProcedure.java
@@ -0,0 +1,31 @@
+package net.mcreator.masterchefrestaurant.procedures;
+
+import net.neoforged.neoforge.event.entity.player.CanPlayerSleepEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.bus.api.Event;
+
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.GameRules;
+
+import javax.annotation.Nullable;
+
+@EventBusSubscriber
+public class PlayerWakesUpProcedure {
+	@SubscribeEvent
+	public static void onPlayerInBed(CanPlayerSleepEvent event) {
+		execute(event, event.getEntity().level());
+	}
+
+	public static void execute(LevelAccessor world) {
+		execute(null, world);
+	}
+
+	private static void execute(@Nullable Event event, LevelAccessor world) {
+		if (!world.isClientSide() && Level.OVERWORLD == (world instanceof Level _lvl ? _lvl.dimension() : (world instanceof WorldGenLevel _wgl ? _wgl.getLevel().dimension() : Level.OVERWORLD))) {
+			world.getLevelData().getGameRules().getRule(GameRules.RULE_DAYLIGHT).set(false, world.getServer());
+		}
+	}
+}
